a19503e44a82249843c6ecdc0ad9c9db73c4d7fc
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-22 11:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
+	str=str.toLoereCase().replace(/[^a-z0-9]/g, "");
+	return syr===str.split("").reverse().join("");
+	
 
 }
 module.exports = palindrome
